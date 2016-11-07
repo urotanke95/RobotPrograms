04adb00edeae11021e92c724b3205b139a0d043c
urotanke95 <kino95@berkeley.edu>
2016-11-07 16:15:27 +0900

    added pinkyu1

--- pinkyu1.cpp
+++ pinkyu1.cpp
@@ -1,244 +1,216 @@
-#include <iostream>
-
-#include<opencv/cvaux.h>
-#include<opencv/highgui.h>
-#include<opencv/cxcore.h>
-
-#include<stdio.h>
-#include<stdlib.h>
-
-// Need to include this for serial port communication
-#include <Windows.h>
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-int main(int argc, char* argv[])
-{
-	// Setup serial port connection and needed variables.
-	HANDLE hSerial = CreateFile(L"COM3", GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
-
-	if (hSerial !=INVALID_HANDLE_VALUE)
-    {
-        printf("Port opened! \n");
-
-        DCB dcbSerialParams;
-        GetCommState(hSerial,&dcbSerialParams);
-
-        dcbSerialParams.BaudRate = CBR_9600;
-        dcbSerialParams.ByteSize = 8;
-        dcbSerialParams.Parity = NOPARITY;
-        dcbSerialParams.StopBits = ONESTOPBIT;
-
-        SetCommState(hSerial, &dcbSerialParams);
-	}
-	else
-	{
-		if (GetLastError() == ERROR_FILE_NOT_FOUND)
-		{
-			printf("Serial port doesn't exist! \n");
-		}
-
-		printf("Error while setting up serial port! \n");
-	}
-
-	char outputChars[] = "c";
-	DWORD btsIO;
-
-	// Setup OpenCV variables and structures
-	CvSize size640x480 = cvSize(640, 480);			// use a 640 x 480 size for all windows, also make sure your webcam is set to 640x480 !!
-
-	CvCapture* p_capWebcam;						// we will assign our web cam video stream to this later . . .
-
-	IplImage* p_imgOriginal;			// pointer to an image structure, this will be the input image from webcam
-	IplImage* p_imgProcessed;			// pointer to an image structure, this will be the processed image
-	IplImage* p_imgHSV;                 // pointer to an image structure, this will hold the image after the color has been changed from RGB to HSV
-										// IPL is short for Intel Image Processing Library, this is the structure used in OpenCV 1.x to work with images
-
-	CvMemStorage* p_strStorage;			// necessary storage variable to pass into cvHoughCircles()
-
-	CvSeq* p_seqCircles;				// pointer to an OpenCV sequence, will be returned by cvHough Circles() and will contain all circles
-										// call cvGetSeqElem(p_seqCircles, i) will return a 3 element array of the ith circle (see next variable)
-	
-	float* p_fltXYRadius;				// pointer to a 3 element array of floats
-										// [0] => x position of detected object
-										// [1] => y position of detected object
-										// [2] => radius of detected object
-
-	int i;								// loop counter
-	char charCheckForEscKey;			// char for checking key press (Esc exits program)
-
-	p_capWebcam = cvCaptureFromCAM(0);	// 0 => use 1st webcam, may have to change to a different number if you have multiple cameras
-
-	if(p_capWebcam == NULL) {			// if capture was not successful . . .
-		printf("error: capture is NULL \n");	// error message to standard out . . .
-		getchar();								// getchar() to pause for user see message . . .
-		return(-1);								// exit program
-	}
-
-											            // declare 2 windows
-	cvNamedWindow("Original", CV_WINDOW_AUTOSIZE);		// original image from webcam
-	cvNamedWindow("Processed", CV_WINDOW_AUTOSIZE);		// the processed image we will use for detecting circles
-
-	p_imgProcessed = cvCreateImage(size640x480,			// 640 x 480 pixels (CvSize struct from earlier)
-								   IPL_DEPTH_8U,		// 8-bit color depth
-								   1);					// 1 channel (grayscale), if this was a color image, use 3
-
-	p_imgHSV = cvCreateImage(size640x480, IPL_DEPTH_8U, 3); 
-
-	// Variables for Arduino Control
-	int servoPosition = 90;
-	int servoOrientation = 0;
-
-	// Main program loop
-	while(1) {								// for each frame . . .
-		p_imgOriginal = cvQueryFrame(p_capWebcam);		// get frame from webcam
-		
-		if(p_imgOriginal == NULL) {					// if frame was not captured successfully . . .
-			printf("error: frame is NULL \n");		// error message to std out
-			getchar();
-			break;
-		}
-
-		// Change the color model from RGB (BGR) to HSV. This makes it easier to choose a color based on Hue
-		cvCvtColor(p_imgOriginal, p_imgHSV, CV_BGR2HSV);
-
-		cvInRangeS(p_imgHSV,				// function input
-				   cvScalar(38,  60,  70),			// min filtering value (if color is greater than or equal to this)
-				   cvScalar(75, 181, 256),			// max filtering value (if color is less than this)
-				   p_imgProcessed);				// function output
-
-		p_strStorage = cvCreateMemStorage(0);	// allocate necessary memory storage variable to pass into cvHoughCircles()
-
-										// smooth the processed image, this will make it easier for the next function to pick out the circles
-		cvSmooth(p_imgProcessed,		// function input
-				 p_imgProcessed,		// function output
-				 CV_GAUSSIAN,			// use Gaussian filter (average nearby pixels, with closest pixels weighted more)
-				 9,						// smoothing filter window width
-				 9);					// smoothing filter window height
-
-													// fill sequential structure with all circles in processed image
-		p_seqCircles = cvHoughCircles(p_imgProcessed,		// input image, nothe that this has to be grayscale (no color)
-									  p_strStorage,			// provide function with memory storage, makes function return a pointer to a CvSeq
-									  CV_HOUGH_GRADIENT,	// two-pass algorithm for detecting circles, this is the only choice available
-									  2,					// size of image / 2 = "accumulator resolution", i.e. accum = res = size of image / 2
-									  p_imgProcessed->height / 4,	// min distance in pixels between the centers of the detected circles
-									  100,						// high threshold of Canny edge detector, called by cvHoughCircles
-									  50,						// low threshold of Canny edge detector, called by cvHoughCircles
-									  10,	 //10					// min circle radius, in pixels
-									  400);						// max circle radius, in pixels
-
-		// Run this if the camera doesn't detect any circles
-		if (p_seqCircles->total == 0)
-		{
-			// Initialize orientation
-			// This just makes it so the camera first goes to the side that it's leaning towards
-			// So if the camera is already mostly facing the left side it goes to the left end
-			// before going to the right. And the other way around.
-			if (servoOrientation == 0)
-			{
-				if (servoPosition >= 90)
-					servoOrientation = 1;
-				else
-					servoOrientation = -1;
-			}
-
-			if (servoOrientation == 1)
-			{
-				outputChars[0] = 'l';
-				WriteFile(hSerial, outputChars, strlen(outputChars), &btsIO, NULL);
-
-				// This code is identical to the one on the Arduino side
-				servoPosition+=5;
-
-				if (servoPosition > 180)
-				{
-					servoPosition = 180;
-					servoOrientation = -1;
-				}
-			}
-			else
-			{
-				outputChars[0] = 'r';
-				WriteFile(hSerial, outputChars, strlen(outputChars), &btsIO, NULL);
-
-				// This code is identical to the one on the Arduino side
-				servoPosition-=5;
-
-				if (servoPosition < 0)
-				{
-					servoPosition = 0;
-					servoOrientation = 1;
-				}
-			}
-		}
-
-		// Run this if the camera can see at least one circle
-		for(i=0; i < p_seqCircles->total; i++) {		// for each element in sequential circles structure (i.e. for each object detected)
-
-			p_fltXYRadius = (float*)cvGetSeqElem(p_seqCircles, i);	// from the sequential structure, read the ith value into a pointer to a float
-
-			printf("ball position x = %f, y = %f, r = %f \n", p_fltXYRadius[0],		// x position of center point of circle
-															  p_fltXYRadius[1],		// y position of center point of circle
-															  p_fltXYRadius[2]);	// radius of circle
-
-			// Reset servo orientation as the camera now has focus of a circle
-			// Servo orientation is important only when the camera doesn't see a circle
-			servoOrientation = 0;
-
-			// Check whether camera should turn to its left if the circle gets near the right end of the screen
-			if (p_fltXYRadius[0] > 540)
-			{
-				outputChars[0] = 'l';
-				WriteFile(hSerial, outputChars, strlen(outputChars), &btsIO, NULL);
-
-				servoPosition+=5;
-
-				if (servoPosition > 180)
-					servoPosition = 180;
-			}
-
-			// Check whether camera should turn to its right if the circle gets near the left end of the screen
-			if (p_fltXYRadius[0] < 100)
-			{
-				outputChars[0] = 'r';
-				WriteFile(hSerial, outputChars, strlen(outputChars), &btsIO, NULL);
-
-				servoPosition-=5;
-
-				if (servoPosition < 0)
-					servoPosition = 0;
-			}
-
-										// draw a small green circle at center of detected object
-			cvCircle(p_imgOriginal,										// draw on the original image
-					 cvPoint(cvRound(p_fltXYRadius[0]), cvRound(p_fltXYRadius[1])),		// center point of circle
-					 3,													// 3 pixel radius of circle
-					 CV_RGB(0,255,0),									// draw pure green
-					 CV_FILLED);										// thickness, fill in the circle
-			
-										// draw a red circle around the detected object
-			cvCircle(p_imgOriginal,										// draw on the original image
-					 cvPoint(cvRound(p_fltXYRadius[0]), cvRound(p_fltXYRadius[1])),		// center point of circle
-					 cvRound(p_fltXYRadius[2]),							// radius of circle in pixels
-					 CV_RGB(255,0,0),									// draw pure red
-					 3);												// thickness of circle in pixels
-		}	// end for
-
-		cvShowImage("Original", p_imgOriginal);			// original image with detectec ball overlay
-		cvShowImage("Processed", p_imgProcessed);		// image after processing
-
-		cvReleaseMemStorage(&p_strStorage);				// deallocate necessary storage variable to pass into cvHoughCircles
-
-		charCheckForEscKey = cvWaitKey(10);				// delay (in ms), and get key press, if any
-		if(charCheckForEscKey == 27) break;				// if Esc key (ASCII 27) was pressed, jump out of while loop
-	}	// end while
-
-	cvReleaseCapture(&p_capWebcam);					// release memory as applicable
-
-	cvDestroyWindow("Original");
-	cvDestroyWindow("Processed");
-
-	// This closes the Serial Port
-    CloseHandle(hSerial);
-
-	return(0);
+// Module "core"
+#include <opencv2/core/core.hpp>
+ 
+// Module "highgui"
+#include <opencv2/highgui/highgui.hpp>
+ 
+// Module "imgproc"
+#include <opencv2/imgproc/imgproc.hpp>
+ 
+// Module "video"
+#include <opencv2/video/video.hpp>
+ 
+// Output
+#include 
+ 
+// Vector
+#include 
+ 
+using namespace std;
+ 
+// >>>>> Color to be tracked
+#define MIN_H_BLUE 30
+#define MAX_H_BLUE 50
+// <<<<< Color to be tracked   int main() {    // Camera frame    cv::Mat frame;      // >>>> Kalman Filter
+   int stateSize = 6;
+   int measSize = 4;
+   int contrSize = 0;
+ 
+   unsigned int type = CV_32F;
+   cv::KalmanFilter kf(stateSize, measSize, contrSize, type);
+ 
+   cv::Mat state(stateSize, 1, type);  // [x,y,v_x,v_y,w,h]
+   cv::Mat meas(measSize, 1, type);    // [z_x,z_y,z_w,z_h]
+   //cv::Mat procNoise(stateSize, 1, type)
+   // [E_x,E_y,E_v_x,E_v_y,E_w,E_h]
+ 
+   // Transition State Matrix A
+   // Note: set dT at each processing step!
+   // [ 1 0 dT 0  0 0 ]
+   // [ 0 1 0  dT 0 0 ]
+   // [ 0 0 1  0  0 0 ]
+   // [ 0 0 0  1  0 0 ]
+   // [ 0 0 0  0  1 0 ]
+   // [ 0 0 0  0  0 1 ]
+   cv::setIdentity(kf.transitionMatrix);
+ 
+   // Measure Matrix H
+   // [ 1 0 0 0 0 0 ]
+   // [ 0 1 0 0 0 0 ]
+   // [ 0 0 0 0 1 0 ]
+   // [ 0 0 0 0 0 1 ]
+   kf.measurementMatrix = cv::Mat::zeros(measSize, stateSize, type);
+   kf.measurementMatrix.at(0) = 1.0f;
+   kf.measurementMatrix.at(7) = 1.0f;
+   kf.measurementMatrix.at(16) = 1.0f;
+   kf.measurementMatrix.at(23) = 1.0f;
+ 
+   // Process Noise Covariance Matrix Q
+   // [ Ex 0  0    0 0    0 ]
+   // [ 0  Ey 0    0 0    0 ]
+   // [ 0  0  Ev_x 0 0    0 ]
+   // [ 0  0  0    1 Ev_y 0 ]
+   // [ 0  0  0    0 1    Ew ]
+   // [ 0  0  0    0 0    Eh ]
+   //cv::setIdentity(kf.processNoiseCov, cv::Scalar(1e-2));
+   kf.processNoiseCov.at(0) = 1e-2;
+   kf.processNoiseCov.at(7) = 1e-2;
+   kf.processNoiseCov.at(14) = 2.0f;
+   kf.processNoiseCov.at(21) = 1.0f;
+   kf.processNoiseCov.at(28) = 1e-2;
+   kf.processNoiseCov.at(35) = 1e-2;
+ 
+   // Measures Noise Covariance Matrix R
+   cv::setIdentity(kf.measurementNoiseCov, cv::Scalar(1e-1));
+   // <<<< Kalman Filter      // Camera Index    int idx = 0;      // Camera Capture    cv::VideoCapture cap;      // >>>>> Camera Settings
+   if (!cap.open(idx))
+   {
+      cout << "Webcam not connected.\n" << "Please verify\n";
+      return EXIT_FAILURE;
+   }
+ 
+   cap.set(CV_CAP_PROP_FRAME_WIDTH, 1024);
+   cap.set(CV_CAP_PROP_FRAME_HEIGHT, 768);
+   // <<<<< Camera Settings
+ 
+   cout << "\nHit 'q' to exit...\n";      char ch = 0;      double ticks = 0;    bool found = false;      int notFoundCount = 0;            // >>>>> Main loop  
+   while (ch != 'q' || ch != 'Q')
+   {
+      double precTick = ticks;
+      ticks = (double) cv::getTickCount();
+ 
+      double dT = (ticks - precTick) / cv::getTickFrequency(); //seconds
+ 
+      // Frame acquisition
+      cap >> frame;
+ 
+      cv::Mat res;
+      frame.copyTo( res );
+ 
+      if (found)
+      {
+         // >>>> Matrix A
+         kf.transitionMatrix.at(2) = dT;
+         kf.transitionMatrix.at(9) = dT;
+         // <<<< Matrix A
+ 
+         cout << "dT:" << endl << dT << endl;
+ 
+         state = kf.predict();
+         cout << "State post:" << endl << state << endl;            cv::Rect predRect;          predRect.width = state.at(4);          predRect.height = state.at(5);          predRect.x = state.at(0) - predRect.width / 2;          predRect.y = state.at(1) - predRect.height / 2;            cv::Point center;          center.x = state.at(0);          center.y = state.at(1);          cv::circle(res, center, 2, CV_RGB(255,0,0), -1);            cv::rectangle(res, predRect, CV_RGB(255,0,0), 2);       }         // >>>>> Noise smoothing
+      cv::Mat blur;
+      cv::GaussianBlur(frame, blur, cv::Size(5, 5), 3.0, 3.0);
+      // <<<<< Noise smoothing         // >>>>> HSV conversion
+      cv::Mat frmHsv;
+      cv::cvtColor(blur, frmHsv, CV_BGR2HSV);
+      // <<<<< HSV conversion         // >>>>> Color Thresholding
+      // Note: change parameters for different colors
+      cv::Mat rangeRes = cv::Mat::zeros(frame.size(), CV_8UC1);
+      cv::inRange(frmHsv, cv::Scalar(MIN_H_BLUE, 100, 80),
+            cv::Scalar(MAX_H_BLUE, 255, 255), rangeRes);
+      // <<<<< Color Thresholding         // >>>>> Improving the result
+      cv::erode(rangeRes, rangeRes, cv::Mat(), cv::Point(-1, -1), 2);
+      cv::dilate(rangeRes, rangeRes, cv::Mat(), cv::Point(-1, -1), 2);
+      // <<<<< Improving the result         // Thresholding viewing       cv::imshow("Threshold", rangeRes);         // >>>>> Contours detection
+      vector contours;
+      cv::findContours(rangeRes, contours, CV_RETR_EXTERNAL,
+            CV_CHAIN_APPROX_NONE);
+      // <<<<< Contours detection         // >>>>> Filtering
+      vector balls;
+      vector ballsBox;
+      for (size_t i = 0; i < contours.size(); i++)       {          cv::Rect bBox;          bBox = cv::boundingRect(contours[i]);            float ratio = (float) bBox.width / (float) bBox.height;          if (ratio > 1.0f)
+            ratio = 1.0f / ratio;
+ 
+         // Searching for a bBox almost square
+         if (ratio > 0.75 && bBox.area() >= 400)
+         {
+            balls.push_back(contours[i]);
+            ballsBox.push_back(bBox);
+         }
+      }
+      // <<<<< Filtering
+ 
+      cout << "Balls found:" << ballsBox.size() << endl;         // >>>>> Detection result
+      for (size_t i = 0; i < balls.size(); i++)
+      {
+         cv::drawContours(res, balls, i, CV_RGB(20,150,20), 1);
+         cv::rectangle(res, ballsBox[i], CV_RGB(0,255,0), 2);
+ 
+         cv::Point center;
+         center.x = ballsBox[i].x + ballsBox[i].width / 2;
+         center.y = ballsBox[i].y + ballsBox[i].height / 2;
+         cv::circle(res, center, 2, CV_RGB(20,150,20), -1);
+ 
+         stringstream sstr;
+         sstr << "(" << center.x << "," << center.y << ")";
+         cv::putText(res, sstr.str(),
+               cv::Point(center.x + 3, center.y - 3),
+               cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(20,150,20), 2);
+      }
+      // <<<<< Detection result         // >>>>> Kalman Update
+      if (balls.size() == 0)
+      {
+         notFoundCount++;
+         cout << "notFoundCount:" << notFoundCount << endl;          if( notFoundCount >= 10 )
+         {
+            found = false;
+         }
+         else
+            kf.statePost = state;
+      }
+      else
+      {
+         notFoundCount = 0;
+ 
+         meas.at(0) = ballsBox[0].x + ballsBox[0].width / 2;
+         meas.at(1) = ballsBox[0].y + ballsBox[0].height / 2;
+         meas.at(2) = (float)ballsBox[0].width;
+         meas.at(3) = (float)ballsBox[0].height;
+ 
+         if (!found) // First detection!
+         {
+            // >>>> Initialization
+            kf.errorCovPre.at(0) = 1; // px
+            kf.errorCovPre.at(7) = 1; // px
+            kf.errorCovPre.at(14) = 1;
+            kf.errorCovPre.at(21) = 1;
+            kf.errorCovPre.at(28) = 1; // px
+            kf.errorCovPre.at(35) = 1; // px
+ 
+            state.at(0) = meas.at(0);
+            state.at(1) = meas.at(1);
+            state.at(2) = 0;
+            state.at(3) = 0;
+            state.at(4) = meas.at(2);
+            state.at(5) = meas.at(3);
+            // <<<< Initialization
+ 
+            found = true;
+         }
+         else
+            kf.correct(meas); // Kalman Correction
+ 
+         cout << "Measure matrix:" << endl << meas << endl;
+      }
+      // <<<<< Kalman Update
+ 
+      // Final result
+      cv::imshow("Risultato finale", res);
+ 
+      // User key
+      ch = cv::waitKey(10);
+   }
+   // <<<<< Main loop
+ 
+   return EXIT_SUCCESS;
 }
