7bdb0199b02dba57cec37c2968defaa97d435075
urotanke95 <kino95@berkeley.edu>
2016-11-14 16:00:42 +0900

    gomi2

--- gomi.cpp
+++ gomi.cpp
@@ -15,27 +15,6 @@ using namespace std;
          return -1;
     }
 
-    namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
-
-  int iLowH = 0;
- int iHighH = 179;
-
-  int iLowS = 0; 
- int iHighS = 255;
-
-  int iLowV = 0;
- int iHighV = 255;
-
-  //Create trackbars in "Control" window
- cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
- cvCreateTrackbar("HighH", "Control", &iHighH, 179);
-
-  cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
- cvCreateTrackbar("HighS", "Control", &iHighS, 255);
-
-  cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
- cvCreateTrackbar("HighV", "Control", &iHighV, 255);
-
     while (true)
     {
         Mat imgOriginal;
@@ -54,7 +33,7 @@ using namespace std;
  
   Mat imgThresholded;
 
-   inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+   inRange(imgHSV, Scalar(200, 0, 0), Scalar(255, 100, 40), imgThresholded); //Threshold the image
       
   //morphological opening (remove small objects from the foreground)
   erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
