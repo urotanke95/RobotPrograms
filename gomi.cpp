ef85dc5800c3a5ecae63140972133ae996e0a6f6
urotanke95 <kino95@berkeley.edu>
2016-11-14 15:16:15 +0900

    gomi

--- gomi.cpp
+++ gomi.cpp
@@ -1,68 +1,123 @@
-#include <iostream>
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-
 using namespace cv;
 using namespace std;
 
+#include "cv.h"
+#include "highgui.h"
+#include "BlobResult.h"
+// Get thresholded image in HSV format
+IplImage* GetThresholdedImageHSV( IplImage* img )
+{
+    // Create an HSV format image from image passed
+    IplImage* imgHSV = cvCreateImage( cvGetSize( img ), 
+                                      8, 
+                                      3 );   
+ 
+    cvCvtColor( img, imgHSV, CV_BGR2HSV );
+ 
+    // Create binary thresholded image acc. to max/min HSV ranges
+    // For detecting blue gloves in "MOV.MPG - HSV mode
+    IplImage* imgThresh = cvCreateImage( cvGetSize( img ), 
+                                         8, 
+                                         1 );           
+ 
+    cvInRangeS( imgHSV,
+                cvScalar( 104, 178, 70  ),
+                cvScalar( 130, 240, 124 ),
+                imgThresh );
+ 
+    // Tidy up and return thresholded image
+    cvReleaseImage( &amp;imgHSV );
+    return imgThresh;
+}
+
+
  int main( int argc, char** argv )
  {
-    VideoCapture cap(0); //capture the video from web cam
-
-    if ( !cap.isOpened() )  // if not success, exit program
-    {
-         cout << "Cannot open the web cam" << endl;
-         return -1;
-    }
-
-    namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
-
-	int iLowH = 200;
-	int iHighH = 255;
-
-	int iLowS = 0; 
-	int iHighS = 100;
-
-	int iLowV = 0;
-	int iHighV = 100;
-	while (true)
-    {
-        Mat imgOriginal;
-
-        bool bSuccess = cap.read(imgOriginal); // read a new frame from video
-
-         if (!bSuccess) //if not success, break loop
-        {
-             cout << "Cannot read a frame from video stream" << endl;
-             break;
-        }
-
-		Mat imgHSV;
-
-		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+	CBlobResult blobs;  
+    CBlob *currentBlob; 
+    CvPoint pt1, pt2;
+    CvRect cvRect;
+    int key = 0;
+    IplImage* frame = 0;
  
-		Mat imgThresholded;
-
-		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-      
-		//morphological opening (remove small objects from the foreground)
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-		dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-
-	   //morphological closing (fill small holes in the foreground)
-		dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-
-		imshow("Thresholded Image", imgThresholded); //show the thresholded image
-		imshow("Original", imgOriginal); //show the original image
-
-        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-       {
-            cout << "esc key is pressed by user" << endl;
-            break; 
-       }
+    // Initialize capturing live feed from video file or camera
+    CvCapture* capture = cvCaptureFromCAM( "MOV.MPG" );
+ 
+    // Get the frames per second
+    int fps = ( int )cvGetCaptureProperty( capture,
+                                           CV_CAP_PROP_FPS );  
+ 
+    // Can't get device? Complain and quit
+    if( !capture )
+    {
+        printf( "Could not initialize capturing...\n" );
+        return -1;
     }
-
-	return 0;
+ 
+    // Windows used to display input video with bounding rectangles
+    // and the thresholded video
+    cvNamedWindow( "video" );
+    cvNamedWindow( "thresh" );      
+ 
+    // An infinite loop
+    while( key != 'x' )
+    {
+        // If we couldn't grab a frame... quit
+        if( !( frame = cvQueryFrame( capture ) ) )
+            break;      
+ 
+        // Get object's thresholded image (blue = white, rest = black)
+        IplImage* imgThresh = GetThresholdedImageHSV( frame );      
+ 
+        // Detect the white blobs from the black background
+        blobs = CBlobResult( imgThresh, NULL, 0 );  
+ 
+        // Exclude white blobs smaller than the given value (10)  
+        // The bigger the last parameter, the bigger the blobs need  
+        // to be for inclusion  
+        blobs.Filter( blobs,
+                      B_EXCLUDE,
+                      CBlobGetArea(),
+                      B_LESS,
+                      10 );         
+ 
+        // Attach a bounding rectangle for each blob discovered
+        int num_blobs = blobs.GetNumBlobs();
+ 
+        for ( int i = 0; i &lt; num_blobs; i++ )  
+        {               
+            currentBlob = blobs.GetBlob( i );             
+            cvRect = currentBlob-&gt;GetBoundingBox();
+ 
+            pt1.x = cvRect.x;
+            pt1.y = cvRect.y;
+            pt2.x = cvRect.x + cvRect.width;
+            pt2.y = cvRect.y + cvRect.height;
+ 
+            // Attach bounding rect to blob in orginal video input
+            cvRectangle( frame,
+                         pt1, 
+                         pt2,
+                         cvScalar(0, 0, 0, 0),
+                         1,
+                         8,
+                         0 );
+        }
+ 
+        // Add the black and white and original images
+        cvShowImage( "thresh", imgThresh );
+        cvShowImage( "video", frame );
+ 
+        // Optional - used to slow up the display of frames
+        key = cvWaitKey( 2000 / fps );
+ 
+        // Prevent memory leaks by releasing thresholded image
+        cvReleaseImage( &amp;imgThresh );      
+    }
+ 
+    // We're through with using camera. 
+    cvReleaseCapture( &amp;capture );
+ 
+    return 0;
 
 }
