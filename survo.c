fee0af222fe7d0ed29d170f95bf177ab38ffa8a1
okku-git <okuokadesukonnnitiha@gmail.com>
2016-12-19 16:09:58 +0900

    survofile

--- survo.c
+++ survo.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
 #include <wiringPi.h>
+#define REV_PWM 107
+#define STOP_PWM 77
+#define POS_PWM 50
 
-int main(){
-
-	if (wiringPiSetupGpio() == -1) {
-		fprintf(stderr, "WiringPi Initialize Error\n");
-		return 0;
-	}
-
+int main()
+{
 	pinMode(18,PWM_OUTPUT);
 	pwmSetMode(PWM_MODE_MS);
 	pwmSetClock(375);
 	pwmSetRange(1024);
 
 	while(1){
-  	int num = 0;
-    int duty = 75;
-		scanf("%i",&num);
-	  printf("%d\n",num);
-		
-		if(num < -90 || 90 < num){
-			fprintf(stderr, "Angle over!\n");
+	    int num;
+	    scanf("%i",&num);
+		printf("%d\n",num);
+				
+		if (num == -1) {
+    		pwmWrite(18, REV_PWM);
+		} else if (num == 0) {
+    		pwmWrite(18, STOP_PWM);
+		} else if (num == 1) {
+    		pwmWrite(18, POS_PWM);
+		} else {
+			printf("end!\n");
 			break;
 		}
-		num += 90;
-		double ratio = num * (97 / 180.0) + 25;
-		printf("%f\n", ratio);
-		duty =(int)ratio;
-		printf("duty:%d\n", duty);
-    pwmWrite(18, duty);
 	}
-  return 0;
+    return 0;
 }
