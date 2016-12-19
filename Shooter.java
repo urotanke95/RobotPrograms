0693a81178edc0bdad5da8e3782093ef57876464
pi <nowda.aki0619@gmail.com>
2016-12-19 17:36:23 +0900

    success Shooter

--- Shooter.java
+++ Shooter.java
@@ -5,15 +5,15 @@ import com.sun.jna.Native;
 public class Shooter {
     public interface ShooterLib extends Library {
         // loadLibraryの第一引数はあとで作成するlib***.soの***と一致させる。
-        String path = "/home/pi/work/robot/RobotPrograms/libshoot.so";
+        String path = "/home/pi/work/robot/RobotPrograms/libshoot2.so";
         ShooterLib INSTANCE = (ShooterLib) Native.loadLibrary(path, ShooterLib.class);
 
         // Cの関数名と一致させる
-        void main();
+        void shoot();
     }
     public static void main(String[] args){
         System.out.println("Shooter");
         ShooterLib shooter = ShooterLib.INSTANCE;
-        shooter.main();
+        shooter.shoot();
     }
 }
