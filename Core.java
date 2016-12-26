d8cd46cbd9a2463cf56f8b96d43c125178ee0bd3
urotanke95 <kino95@berkeley.edu>
2016-12-26 15:13:20 +0900

    test core

--- Core.java
+++ Core.java
@@ -3,16 +3,37 @@ class Core implements ICore {
 
     public static void main(String[] args){
         Core core = new Core();
-        /* 別スレッドとして動作させるオブジェクトを作成 */
+        
         SubThread sub = new SubThread(core);
-        /* 別のスレッドを作成し、スレッドを開始する */
+        
         Thread thread = new Thread(sub);
         thread.start();
     }
 
     public void Order(List<JuliusWord> jwl){
-        for (JuliusWord jw : jwl) {
-            System.out.println("CliantTester: " +" WORD="+ jw.Word_ + " PHONE=" + jw.Phone_);
-        }
+		String[] words = new String[30];
+		int i = 0, j = 0;
+        i = 0;
+		while(words[j] != "silE") {
+			if (words[j].equals("GO")) {
+				PinkyuTester pinkyu1 = new PinkyuTester();
+				pinkyu1.main(null);
+				j = 0;
+				break;
+			} else if (words[j].equals("back")) {
+				GomiTester gomi = new GomiTester();
+				gomi.main(null);
+				j = 0;
+				break;
+			} else if (words[i].equals("SHOT")) {
+				Shooter shoot = new Shooter();
+				shoot.main(null);
+				j = 0;
+				break;
+			} else {
+				j++;
+			}
+		}
+		return;
     }
 }
