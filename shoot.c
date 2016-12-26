a2cfdc7ac2f6c362c7667eafba5dcca6b8e093d8
pi <nowda.aki0619@gmail.com>
2016-12-26 12:40:12 +0900

    move success

--- shoot.c
+++ shoot.c
@@ -3,33 +3,34 @@
 
 int main()
 {
-	int input;
+  int input;
 
-	if (wiringPiSetupGpio() == -1)
-		return 1;
-	printf("Please input 1 to shoot the ball\n");
-	scanf("%d", &input);
+  if (wiringPiSetupGpio() == -1)
+    return 1;
+	
+  while (1) {
 
-	if (input == 1) {
-		pinMode( 04, OUTPUT );
-		pinMode( 17, OUTPUT );
-		pinMode(27, OUTPUT);
-		pinMode(22, OUTPUT);
+    printf("Please input 1 to shoot the ball\n");
+    scanf("%d", &input);
 
-		digitalWrite(04, 1);
-		digitalWrite( 17, 1 );
-		digitalWrite(27, 1);
-		digitalWrite(22, 1);
-	}
+    if (input == 1) {
+      pinMode( 04, OUTPUT );
 
-	printf("Please input 0 to stop the motors\n");
-	scanf("%d", &input);
-	if (input == 0) {
-	  digitalWrite(04, 0);
-	  digitalWrite(17, 0);
-	  digitalWrite(27, 0);
-	  digitalWrite(22, 0);
-	}
+      digitalWrite(04, 1);
+      break;
+    }else
+      continue;
+  }
 
-	return 0;
+  while (1) {
+    printf("Please input 0 to stop the motors\n");
+    scanf("%d", &input);
+    if (input == 0) {
+      digitalWrite(04, 0);
+      break;
+    }else
+      continue;
+  }
+
+  return 0;
 }
