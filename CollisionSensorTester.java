43a3bfaf50babacbe6b492c5e00b66ab362372bb
Imada_Akihiro <nowda.aki0619@gmail.com>
2017-01-23 15:59:03 +0900

    CollisionSensorTester

--- CollisionSensorTester.java
+++ CollisionSensorTester.java
@@ -4,6 +4,10 @@ class CollisionSensorTester{
         Thread sensorThread = new Thread(collisionSensor);
         sensorThread.start();
 
+        try{
+        Thread.sleep(3000); //3000ミリ秒Sleepする
+        }catch(InterruptedException e){}
+
         System.out.println("Sensor Left =" + CollisionSensor.GetLeft());
         System.out.println("Sensor Right =" + CollisionSensor.GetRight());
     }
