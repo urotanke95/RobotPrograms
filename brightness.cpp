e5996ffac646a771849fa81d8e38d2880991ffcd
Imada_Akihiro <nowda.aki0619@gmail.com>
2017-01-27 11:09:51 +0900

    update brightness

--- brightness.cpp
+++ brightness.cpp
@@ -10,14 +10,28 @@ int main(int, char**)
     if(!cap.isOpened())  // 成功したかどうかをチェック
         return -1;
 
-    Mat edges;
-    namedWindow("edges",1);
+    Mat brightness;
+    namedWindow("brightness",1);
     for(;;)
     {
         Mat frame;
         cap >> frame; // カメラから新しいフレームを取得
-        cv::cvtColor(frame, edges, CV_BGR2GRAY);
-        imshow("edges", edges);
+        cv::cvtColor(frame, brightness, CV_BGR2GRAY);
+
+        int intensity, max_intensity;
+        int max_x, max_y;
+        for( int y = 0; y < frame.rows; y++ ) {
+            for( int x = 0; x < frame.cols; x++ ) {
+                intensity = frame.at<unsigned char>(y, x); //X座標がx, Y座標がyに位置するピクセルの値を取得
+                if(!(intensity>max_intensity)){continue;}
+                // intensity is more than max_intensity
+                max_intensity = intensity;
+                max_x = x;
+                max_y = y;
+            }
+        }
+        printf("max is %d at <%d,%d>\n", max_intensity,max_x,max_y);
+        imshow("brightness", brightness);
         if(waitKey(30) >= 0) break;
     }
     // VideoCapture デストラクタにより，カメラは自動的に終了処理されます
