82931ea9fcc15cb89644a99f9087dd41deb30855
Imada_Akihiro <nowda.aki0619@gmail.com>
2017-01-31 16:07:04 +0900

    update brightness.cpp

--- BrightnessApp.java
+++ BrightnessApp.java
@@ -5,7 +5,7 @@ class BrightnessApp implements IApplication {
         Robot.Init();
         System.out.println("Brightness Ready !");
         AudioLib.INSTANCE.play("Brightness.wav");
-        BrightnessLib.INSTANCE.quit_brightness();
+        BrightnessLib.INSTANCE.quit();
     }
 
     public void Quit(){
